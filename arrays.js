488050dcd1834aaf0ea4a2b2ed2aeee5035d0f6e
Easwari Ramani <eshuprad@gmail.com>
2017-09-29 16:50:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(array,element){
 
 };
 function accessElementInArray(array,index){
-  console.log(array[index]);
+  return array[index];
 };
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
