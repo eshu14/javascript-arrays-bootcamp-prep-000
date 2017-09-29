7e02bd8f60343f5cc86c751e8db6d8439af8c0be
Easwari Ramani <eshuprad@gmail.com>
2017-09-29 10:53:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,9 +2,11 @@ var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray(arr,element){
   arr1=[...arr];
   arr1.unshift(element);
+  return arr1;
 };
 function destructivelyAddElementToBeginningOfArray(arr,element){
   arr.push(element);
+  return arr;
 };
 function addElementToEndOfArray(arr,element){
 
